dc96386566876e9f38c29eeb9bb33796c3687171
dave21-py <dgedd236@students.bju.edu>
2025-04-12 04:27:53 -0400

    Added tetris block to Pane

--- app/src/main/java/app/GameWindow.java
+++ app/src/main/java/app/GameWindow.java
@@ -2,14 +2,18 @@ package app;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.util.Duration;
 
 
 public class GameWindow {
+
+    @FXML
+    private Pane gameArea;
     
     @FXML
     public void initialize() {
@@ -23,7 +27,16 @@ public class GameWindow {
                 mediaPlayer.seek(Duration.ZERO);
                 mediaPlayer.play();
             }
-        }); 
+        });
+        ImageView tetrisBlock = new ImageView();
+        Image blockImage = new Image(getClass().getResource("/app/tetris_shape.png").toExternalForm());
+        tetrisBlock.setImage(blockImage);
+        tetrisBlock.setFitHeight(70);
+        tetrisBlock.setFitWidth(70);
+        tetrisBlock.setPreserveRatio(false);
+        tetrisBlock.setX(135);
+        tetrisBlock.setY(225);
+        gameArea.getChildren().add(tetrisBlock);
     }
 
     @FXML 
